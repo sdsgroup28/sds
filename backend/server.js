6714d50c3f65c7f5967944a6b698c0ba4d1ed903
sdsgroup28 <sdsgroup28@gmail.com>
2026-03-31 20:08:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,3 +44,8 @@ app.use('/api/categories', categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req,res)=>
+    {
+        res.send("Backend running!")
+    })
